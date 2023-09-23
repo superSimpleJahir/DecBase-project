596f9de880bcaa92f5b6d823f45b327a61cf3723
superSimpleJahir <supersimple.jahir@gmail.com>
2023-09-23 22:55:44 +0600

    first commit

---
+++ script.js
@@ -0,0 +1,10 @@
+// problem - 1: write an object inside an array.And try to access it.
+
+// problem - 2: write an array inside an object.And try to access it.
+
+// problem - 3: Give an example of Array Map Method.
+
+// problem - 4: Push something in LocalStorage and bring it back.
+
+// problem - 5: iterate over the elements of an array in JavaScript.
+
